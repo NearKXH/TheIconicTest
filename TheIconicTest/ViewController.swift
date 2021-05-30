c784debddc4dfa1ba4d649b745275745549d98e9
Near Kong <near.kongxh@139.com>
2021-05-31 09:31:29 +1000

    initiative project

---
+++ TheIconicTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheIconicTest
+//
+//  Created by Near Kong on 2021/5/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
