8151bbb9321693fd28d9c980b44baa8590b0cfd0
Near Kong <near.kongxh@139.com>
2021-05-31 13:39:28 +1000

    setup catalog view model

--- TheIconicTest/Interface/Modules/Catalog/CatalogViewModel.swift
+++ TheIconicTest/Interface/Modules/Catalog/CatalogViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+import RxSwift
+import RxCocoa
+
+struct CatalogViewModel: ViewModelProtocol {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+//        let dataSource: Driver<[CatalogProductVMModel]>
+    }
+    
+    let input: Input
+    let output: Output
+    
+    private let models = BehaviorRelay<[ProductModel]>(value: [])
+    private var page: Int = 1
+    
+    private let disposeBag = DisposeBag()
+    
+    init(input: Input) {
+        self.input = input
+        self.output = Output()
+    }
+    
+}
