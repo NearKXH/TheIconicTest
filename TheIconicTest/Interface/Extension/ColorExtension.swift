b435f5db08a2109754c53af4da03f4d8c4cc1308
Near Kong <near.kongxh@139.com>
2021-05-31 16:23:25 +1000

    add color extension

---
+++ TheIconicTest/Interface/Extension/ColorExtension.swift
@@ -0,0 +1,28 @@
+//
+//  ColorExtension.swift
+//  TheIconicTest
+//
+//  Created by Near Kong on 2021/5/31.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var blueColor: UIColor {
+        return UIColor(red: 66.0 / 255.0, green: 171.0 / 255.0, blue: 200.0 / 255.0, alpha: 1.0)
+    }
+    
+    static var discountColor: UIColor {
+        return UIColor(red: 239.0 / 255.0, green: 66.0 / 255.0, blue: 60.0 / 255.0, alpha: 1.0)
+    }
+    
+    static var priceColor: UIColor {
+        return UIColor(white: 175.0 / 255.0, alpha: 1.0)
+    }
+    
+    static var textColor: UIColor {
+        return UIColor(white: 105.0 / 255.0, alpha: 1.0)
+    }
+    
+}
