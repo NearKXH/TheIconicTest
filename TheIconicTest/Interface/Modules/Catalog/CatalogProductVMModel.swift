a559d7b9dd8e9e921103f1ad68b20ebe9e6777e3
Near Kong <near.kongxh@139.com>
2021-05-31 18:02:05 +1000

    add image & like observicable

--- TheIconicTest/Interface/Modules/Catalog/CatalogProductVMModel.swift
+++ TheIconicTest/Interface/Modules/Catalog/CatalogProductVMModel.swift
@@ -8,6 +8,9 @@
 import Foundation
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 class CatalogProductVMModel {
     let sku: String
     let final_price: String
@@ -17,11 +20,34 @@ class CatalogProductVMModel {
     let imageUrl: String
     let brandName: String
     
-    var images: UIImage? = nil
+    var imageObservable: Driver<UIImage?> {
+        if imageBehavior.value == nil {
+            downloadImage()
+        }
+        return imageBehavior.asDriver()
+    }
+    
+    private lazy var imageBehavior: BehaviorRelay<UIImage?> = {
+        BehaviorRelay<UIImage?>(value: nil)
+    }()
+    
+    private let disposeBag = DisposeBag()
+    
+    private func downloadImage() {
+        if let imageRequest = ImageNetwork(url: imageUrl) {
+            NetworkManager.manager.rx.downloadImage(imageRequest).subscribe { [unowned self] (result) in
+                if case let .success(image) = result {
+                    imageBehavior.accept(image)
+                }
+            }.disposed(by: disposeBag)
+        }
+    }
+    
+    let likeObservable = BehaviorRelay(value: false)
     
     init(product: ProductModel) {
         sku = product.sku ?? ""
-        final_price = String(format: "%.2f", product.final_sale ?? 0.0)
+        final_price = String(format: "%.2f", product.final_sale ?? product.price ?? 0.0)
         price = String(format: "%.2f", product.price ?? 0.0)
         name = product.name ?? ""
         short_description = product.short_description ?? ""
