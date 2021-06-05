4b6f0b9469b57d0e6cad09ba4f9ca1234a6babbb
Near Kong <near.kongxh@139.com>
2021-06-05 23:46:56 +1000

    fix network error

--- TheIconicTest/Interface/Modules/Catalog/CatalogViewModel.swift
+++ TheIconicTest/Interface/Modules/Catalog/CatalogViewModel.swift
@@ -27,37 +27,78 @@ class CatalogViewModel: ViewModelProtocol {
     private var page: Int = 0
     private var refreshBeginStatus = RefreshBeginStatus.none
     
-    private let dataSource = BehaviorRelay<[CatalogProductVMModel]>(value: [])
-    private let refresh = BehaviorRelay(value: RefreshEndStatus.normal)
-    
-    private let networkBehaviorRelay: BehaviorRelay<Single<[ProductModel]>> = {
-        let single = Single<[ProductModel]>.create { (single) -> Disposable in
-            single(.success([]))
-            return Disposables.create {}
-        }
-        return BehaviorRelay(value: single)
-    }()
-    
     private let disposeBag = DisposeBag()
     private let network = NetworkManager.manager
     
     required init(input: Input) {
+        let dataSource = BehaviorRelay<[CatalogProductVMModel]>(value: [])
+        let refreshEndStatus = BehaviorRelay(value: RefreshEndStatus.normal)
+        
         self.input = input
-        self.output = Output(dataSource: dataSource.asDriver(onErrorJustReturn: []), refresh: refresh.asDriver(onErrorJustReturn: .normal))
+        self.output = Output(dataSource: dataSource.asDriver(onErrorJustReturn: []), refresh: refreshEndStatus.asDriver(onErrorJustReturn: .normal))
+        
+        let networkBehaviorRelay: BehaviorRelay<Single<[ProductModel]>> = {
+            let single = Single<[ProductModel]>.create { (single) -> Disposable in
+                single(.success([]))
+                return Disposables.create {}
+            }
+            return BehaviorRelay(value: single)
+        }()
         
         /// init finished
-        networkBehaviorObserve()
-
-        input.refresh.subscribe(onNext: { [unowned self] (refresh) in
-            if case .error(_) = self.refresh.value {
-                // The request of Header is error
-                // send it again
-                networkBehaviorObserve()
-                if refreshBeginStatus == .header || refreshBeginStatus == refresh {
-                    return
+        networkBehaviorRelay.flatMapLatest({ (single) in
+            return single.map { (products) in
+                Result<[ProductModel], Error>.success(products)
+            }.asInfallible { (error) -> Infallible<Result<[ProductModel], Error>> in
+                Infallible.create { (infallible) -> Disposable in
+                    infallible(InfallibleEvent.next(Result.failure(error)))
+                    return Disposables.create {}
                 }
             }
-            
+        }).map { (result) -> Result<[CatalogProductVMModel], Error> in
+            switch result {
+            case let .success(products):
+                return .success(products.map({ (product) in
+                    CatalogProductVMModel(product: product)
+                }))
+            case let .failure(error):
+                return .failure(error)
+            }
+        }.subscribe(onNext: { [unowned self] (result) in
+            switch result {
+            case let .success(products):
+                switch refreshBeginStatus {
+                case .header:
+                    dataSource.accept(products)
+                case .footer:
+                    dataSource.accept(dataSource.value + products)
+                case .none:
+                    refreshEndStatus.accept(.normal)
+                    return
+                }
+
+                page = page + 1
+                refreshBeginStatus = .none
+                refreshEndStatus.accept( products.count < CatalogNetwork.pageSize ? .noMoreData : .normal)
+                
+            case let .failure(error):
+                print(error)
+                switch refreshBeginStatus {
+                case .header:
+                    dataSource.accept([])
+                    refreshEndStatus.accept(.error(refreshBeginStatus))
+                case .footer:
+                    refreshEndStatus.accept(.error(refreshBeginStatus))
+                case .none:
+                    refreshEndStatus.accept(.normal)
+                }
+                
+                refreshBeginStatus = .none
+            }
+
+        }).disposed(by: disposeBag)
+
+        input.refresh.subscribe(onNext: { [unowned self] (refresh) in
             switch refresh {
             case .header:
                 page = 0
@@ -71,41 +112,6 @@ class CatalogViewModel: ViewModelProtocol {
             case .none:
                 break
             }
-        }).disposed(by: disposeBag)
-    }
-    
-    private func networkBehaviorObserve() {
-        networkBehaviorRelay.flatMapLatest({ $0 }).map({ (products) in
-            products.map { (product) in
-                CatalogProductVMModel(product: product)
-            }
-        }).subscribe(onNext: { [unowned self] (products) in
-            switch refreshBeginStatus {
-            case .header:
-                dataSource.accept(products)
-            case .footer:
-                dataSource.accept(dataSource.value + products)
-            case .none:
-                refresh.accept(.normal)
-                return
-            }
-            
-            page = page + 1
-            refreshBeginStatus = .none
-            refresh.accept( products.count < CatalogNetwork.pageSize ? .noMoreData : .normal)
-            
-        }, onError: { [unowned self] (error) in
-            print(error)
-            switch refreshBeginStatus {
-            case .header:
-                dataSource.accept([])
-                refresh.accept(.error(refreshBeginStatus))
-            case .footer:
-                refresh.accept(.error(refreshBeginStatus))
-            case .none:
-                refresh.accept(.normal)
-            }
-
         }).disposed(by: disposeBag)
     }
     
